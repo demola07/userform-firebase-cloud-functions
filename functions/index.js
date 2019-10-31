a8e2b391d5a53a366c84360c41cb2f79485050ee
demola <demolasobaki@gmail.com>
2019-10-31 19:09:18 +0100

    Firebase cloud function with oncreate listener

---
+++ functions/index.js
@@ -0,0 +1,57 @@
+const functions = require('firebase-functions');
+
+const express = require('express');
+const cors = require('cors');
+const uuidv5 = require('uuid/v4');
+
+const app = express();
+app.use(cors());
+
+//Import and initialize firebase realtime database
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+app.post('/', async (req, res) => {
+    const entry = req.body
+
+    await admin.database().ref('/entries/').push(entry)
+        .then(() => {
+            return res.status(200).json({
+                status: true,
+                data: entry,
+                message: 'Data saved successfully'
+            })
+        })
+        .catch((error) => {
+            console.error(error);
+            return res.status(500).json({
+                status: false,
+                message: 'Error' + error
+            })
+        })
+})
+
+// app.get('/', (req, res) => {
+//     return admin.database().ref('/entries').on("value", snapshot => {
+//         return res.status(200).json({
+//             status: 200,
+//             data: snapshot.val()
+//         });
+//     }, error => {
+//         console.error(error);
+//         return res.status(500).send('Oh no! Error: ' + error);
+//     });
+// })
+exports.entries = functions.https.onRequest(app)
+
+
+exports.onMessageCsreate = functions.database
+    .ref('/entries/{entryId}')
+    .onCreate((snapshot, context) => {
+        // const entryId = context.params.pushId
+        // console.log(entryId);
+
+        const messageData = snapshot.val()
+        messageData.uid = uuidv5('hello.example.com', uuidv5.DNS);
+        return snapshot.ref.update(messageData)
+    })
